202a51d39f98d5e75903ca2ec3174ec3c7efc184
Abhinav20006 <143079865+Abhinav20006@users.noreply.github.com>
2023-09-20 16:23:43 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,73 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//aumto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
+global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
+global.antispam = false //auto kick spammer (true to on, false to off)
+
+
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "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"
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['919446830008'] //ur owner number
+global.ownernomer = "919446830008" //ur owner number2
+global.ownername = "𝐀𝐁𝐇𝐈𝐍𝐀𝐕" //ur owner name
+global.ytname = "YT: abhinav" //ur yt chanel name
+global.socialm = "insta: sp.arkle" //ur github or insta name
+global.location = "India,kerala,palakkad" //ur location
+
+//new
+global.botname = "𝐙𝐘𝐑𝐎𝐗"
+global.ownernumber = '919446830008' 
+global.ownername = '𝐀𝐁𝐇𝐈𝐍𝐀𝐕'
+global.ownerNumber = ["919446830008@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@DGXeon"
+global.websitex = "https://youtu.be/L_SIk59QeAU"
+global.wagc = "https://instagram.com/sp.arkle._0"
+global.themeemoji = '🪀'
+global.wm = "Xeon Bot Inc."
+global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.packname = "𝐙𝐘𝐑𝐎𝐗"
+global.author = "𝐀𝐁𝐇𝐈𝐍𝐀𝐕❤️💫\n\n"
+global.creator = "917002015750@s.whatsapp.net"
+global.prefa = ['.']
+global.sessionName = 'session'
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("https://i.imgur.com/i5lcy0T.jpeg") //ur thumb pic
+global.log0 = fs.readFileSync("https://i.imgur.com/lWIOnUG.jpeg") //ur logo pic
+global.err4r = fs.readFileSync("https://i.imgur.com/gZyoLEK.jpeg") //ur error pic
+global.thumb = fs.readFileSync("https://i.imgur.com/GzQfNAO.jpeg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: 'Here you go!', 
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
